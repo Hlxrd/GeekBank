1b5e7f42dd1f7d80b272dc9648193bcda66553f4
youness <younessaithadou9@gmail.com>
2024-04-02 14:26:27 +0000

    add admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Pest\Plugins\Profile;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        User::insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin'),
+            'seller_or_buyer' => 'seller',
+            // 'double_auth_permition' => 'chi7aja',
+            // 'double_auth_code' => 'chi7aja',
+            // 'double_auth_expires_at' => 'chi7aja',
+            // 'email_verified_at' => 'chi7aja',
+        ]);
+    }
+}
