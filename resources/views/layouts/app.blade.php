3833af2266d871b4d0d32fdbe38d8d24d227eb60
youness <younessaithadou9@gmail.com>
2024-04-23 00:14:36 +0100

    fix scrolling bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,10 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-<link rel="./sass/home.scss" href="">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <link rel="./sass/home.scss" href="">
 </head>
 
 <body class="">
@@ -21,13 +23,13 @@
         @include('layouts.navigation')
 
         @include('layouts.flash')
-        
+
         <!-- Page Content -->
         <div class="flex ">
-        @include('layouts.sidebar')            
-        <main class="p-[2rem] bg-gray-900 w-full position-relative">
-            {{ $slot }}
-        </main>
+            @include('layouts.sidebar')
+            <main class="p-[2rem] bg-gray-400 w-full  position-relative mt-[13vh] ms-[20vw]">
+                {{ $slot }}
+            </main>
         </div>
 
     </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,32 +1,35 @@
-<nav class="bg-gray-900 h-[13vh] flex items-center">
+<div class="fixed top-0 right-0 left-0 z-30">
+    <nav class="bg-gray-900 h-[13vh] flex items-center ">
 
-    <div class="px-10 flex w-full flex-row justify-between">
-        <h2 class="text-white text-4xl">Geek<span class="text-secondary-color">Bank.</span></h2>
-
-
-
-    </div>
-    {{-- <div class="w-[15vw] ">
-    <div class="hover:bg-secondary-color transition-all bg-green-600  border border-black w-[10vw] h-[7vh] rounded-full">
-        <h1 class="text-center text-white text-xl py-2">1500DH</h1>
-    </div>
-</div> --}}
+        <div class="px-10 flex w-full flex-row justify-between">
+            <h2 class="text-white text-4xl">Geek<span class="text-secondary-color">Bank.</span></h2>
 
 
-    <div class="w-[15vw] px-4">
-        <div class="dropdown bg-secondary-color rounded-full hover:cursor-pointer">
-            <button
-                class="btn border-none dropdown-toggle inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 transition duration-150 ease-in-out bg-transparent hover:text-gray-500 bg-secondary-color"
-                type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                {{ Auth::user()->name }}
-            </button>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="{{ route('transaction.index') }}">History</a></li>
-                <li><a class="dropdown-item" href="{{ route('profile.edit') }}">Settings</a></li>
-            </ul>
+
         </div>
-    </div>
+        {{-- <div class="w-[15vw] ">
+        <div class="hover:bg-secondary-color transition-all bg-green-600  border border-black w-[10vw] h-[7vh] rounded-full">
+            <h1 class="text-center text-white text-xl py-2">1500DH</h1>
+        </div>
+    </div> --}}
+
+
+        <div class="w-[15vw] px-4">
+            <div class="dropdown bg-secondary-color rounded-full hover:cursor-pointer">
+                <button
+                    class="btn border-none dropdown-toggle inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 transition duration-150 ease-in-out bg-transparent hover:text-gray-500 bg-secondary-color"
+                    type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    {{ Auth::user()->name }}
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="{{ route('transaction.index') }}">History</a></li>
+                    <li><a class="dropdown-item" href="{{ route('profile.edit') }}">Settings</a></li>
+                </ul>
+            </div>
+        </div>
 
 
 
-</nav>
+    </nav>
+
+</div>
